695d24d666ce1716165e258c8f6f6101e96bc237
Paul Denya <paul@hellofax.com>
2013-08-11 00:17:50 -0700

    imported site, such as it is

---
+++ site/index.php
@@ -0,0 +1,76 @@
+<? 
+$root = "";
+include "header.php"; 
+?>
+
+<!-- hero img and header -->
+<div class="container" id="header_row">
+	<div class="row">
+		<div class="col-lg-4 center">
+			<img src="img/iphone_angled.png" width="273" height="711" id="iphone_angled" />
+			<!-- Thanks to http://placeit.breezi.com/ for generating this image. -->
+			<!-- TODO: put this somewhere visible on the site when there's more content -->
+		</div>
+		<div class="col-lg-8 voffset8">
+			<h1>Try Status</h1>
+			<h3>A fresh slice of your Facebook timeline</h3>
+		</div>
+	</div>
+</div>
+
+<!-- blue band -->
+<div class="container-fluid blue-band">
+	<div class="container">
+		<div class="row voffset5">
+			<div class="col-lg-8 col-lg-offset-4">
+				
+				
+					<h3>Status is currently in closed beta.</h3>
+
+					<!--signup form-->
+					<form action="http://nextmarvel.us2.list-manage1.com/subscribe/post?u=c8e2aa54a2adc040180e79b9a&amp;id=e69532e733" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div class="container-fluid">
+							<div class="row voffset4">
+								<div class="col-lg-8">
+									<input type="email" name="EMAIL" class="form-control input-lg" placeholder="email@address.com" />
+								</div>
+								<div class="col-lg-4">
+									<input type="submit" class="btn btn-primary btn-lg" value="Send me updates" />
+								</div>
+							</div>
+						</div>
+					</form>
+
+			</div>
+		</div>
+	</div><!--container-->
+</div><!--container-fluid blue-band-->
+
+<div class="container">
+	<div class="row voffset5">
+		<div class="col-lg-8 col-lg-offset-4">
+			<h2>Keep track of your friends</h2>
+			<p>
+				The Facebook timeline doesn't show every status post from all of your friends.  There's a lot of 
+				other content to display and they try to display the best.  Status helps you keep track of updates
+				in lives of friends that would otherwise slip through the cracks.  
+			</p>
+
+			<h3 class="voffset4">All statuses</h3>
+			<p>All the status posts from your friends, not just the ones we think are best and not just the ones you've interacted with in the past few weeks. That said, you can filter individuals easily within the app.</p>
+
+			<h3 class="voffset5">Updates in the order they were posted</h3>
+			<p>Posts are displayed in chronological order. Always. Without having to reselect "Recent Posts" once a month.</p>
+
+			<h3 class="voffset4">Only statuses</h3>
+			<p>Photos, links, check-ins and other types of post content aren't displayed at all in favor of showing the most relevant content from your friends</p>
+
+			<h3 class="voffset4">Keep the conversation going</h3>
+			<p>Our Conversation List shows only posts with comments that you haven't read yet. Once you read the comments the post is removed from the list until a new comment is added.</p>
+		</div>
+	</div>
+</div>
+
+<!--footer-->
+	</body>
+</html>
